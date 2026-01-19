d90b569e859e4a24edd3b130168952653a492f1b
Zhibek Zhagypar <zhibekazhibeka@gmail.com>
2026-01-19 13:42:52 +0000

    Added check with GUID for ChangeStatus() and Delete(). Corrected ShowAll() and Filter() for showing Description and date

--- Program.cs
+++ Program.cs
@@ -36,8 +36,8 @@ namespace Lessons {
         public TaskBoard() {
             tasks = new List<TaskItem>();
         }
-        public void AddTask (string name, TaskStatus status) {
-            var task = new TaskItem(name, "", status, DateTime.Now, Guid.NewGuid().ToString());
+        public void AddTask (string name, string description, TaskStatus status) {
+            var task = new TaskItem(name, description, status, DateTime.Now, Guid.NewGuid().ToString());
             tasks.Add(task);
         }
         public void ShowAll() {
@@ -46,7 +46,7 @@ namespace Lessons {
                 return;
             }
             foreach (var task in tasks) {
-                Console.WriteLine($"Task: {task.GetTaskName()}, Status: {task.GetStatus()}");
+                Console.WriteLine($"Task: {task.GetTaskName()}, Description: {task.Description}, Status: {task.GetStatus()}, Created At: {task.date}, ID: {task.GUID}");
             }
         }
         public List<TaskItem> Filter(TaskStatus status) {
@@ -56,20 +56,20 @@ namespace Lessons {
                 return result;
             }
             foreach (var task in result) {
-                Console.WriteLine($"Task: {task.GetTaskName()}, Status: {task.GetStatus()}");
+                Console.WriteLine($"Task: {task.GetTaskName()}, Description: {task.Description}, Status: {task.GetStatus()}, Created At: {task.date}, ID: {task.GUID}");
             }
             return result;
         }
-        public void ChangeStatus(string taskName, TaskStatus newStatus) {
-            var task = tasks.FirstOrDefault(t => t.TaskName == taskName);
+        public void ChangeStatus(string taskIDToChange, TaskStatus newStatus) {
+            var task = tasks.FirstOrDefault(t => t.GUID == taskIDToChange);
             if (task != null) {
                 task.status = newStatus;
             } else {
                 Console.WriteLine("Task not found.");
             }
         }
-        public void Delete(string taskName) {
-            var task = tasks.FirstOrDefault(t => t.TaskName == taskName);
+        public void Delete(string taskIDToDelete) {
+            var task = tasks.FirstOrDefault(t => t.GUID == taskIDToDelete);
             if (task != null) {
                 tasks.Remove(task);
             } else {
@@ -89,12 +89,14 @@ namespace Lessons {
                     case "1":
                         Console.Write("Enter task name: ");
                         string taskName = Console.ReadLine();
+                        Console.Write("Enter description for this task: ");
+                        string description = Console.ReadLine();
                         Console.Write("Enter task status (ToDo, InProgress, Done): ");
                         TaskStatus status;
                         while (!Enum.TryParse(Console.ReadLine(), out status)) {
                             Console.Write("Invalid status. Enter again (ToDo, InProgress, Done): ");
                         }
-                        myTasks.AddTask(taskName, status);
+                        myTasks.AddTask(taskName, description, status);
                         break;
                     case "2": 
                         myTasks.ShowAll(); 
@@ -108,19 +110,20 @@ namespace Lessons {
                         myTasks.Filter(filterStatus);
                         break;
                     case "4": 
-                        Console.Write("Enter task name: ");
-                        taskName = Console.ReadLine();
+                        Console.Write("Enter task ID to change status: ");
+                        string taskIDToChange = Console.ReadLine();
                         TaskStatus newStatus;
+                        Console.Write("Enter new status (ToDo, InProgress, Done): ");
                         while (!Enum.TryParse(Console.ReadLine(), out newStatus)) {
                             Console.Write("Invalid status. Enter again (ToDo, InProgress, Done): ");
                         }
-                        myTasks.ChangeStatus(taskName, newStatus);
+                        myTasks.ChangeStatus(taskIDToChange, newStatus);
                         break;
                     case "5": 
-                        string taskToDelete;
-                        Console.Write("Enter task name to delete: ");
-                        taskToDelete = Console.ReadLine();
-                        myTasks.Delete(taskToDelete); 
+                        string taskIDToDelete;
+                        Console.Write("Enter task ID to delete: ");
+                        taskIDToDelete = Console.ReadLine();
+                        myTasks.Delete(taskIDToDelete); 
                         break;
                     case "0": return;
                     default: Console.WriteLine("Unknown option."); break;
